0e026b1946c95e43d2af44d98259c8c9833e77d7
HoussemDellai <houssem.dellai@live.com>
2014-06-14 18:04:23 +0100

    Fixed more layout on the videos page for WP8 app.

---
+++ Ferrari.WPhone8/Converters/PortraitImageConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Ferrari.Models;
+
+namespace Ferrari.Converters
+{
+	public class PortraitImageConverter : IValueConverter
+	{
+		public object Convert(object value,
+			Type targetType,
+			object parameter,
+			CultureInfo culture)
+		{
+			var imagesCollection = value as List<FlickrImage>;
+
+			if (imagesCollection == null) return null;
+
+			return imagesCollection.FirstOrDefault(image => image.LargeImageWidth < image.LargeImageHeight).LargeImageUrl;
+		}
+
+		public object ConvertBack(object value,
+			Type targetType,
+			object parameter,
+			CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
